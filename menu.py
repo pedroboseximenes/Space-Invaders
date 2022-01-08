feb7789f031b8dfff727431b86ea5e8dd986cc56
Pedro Bose Ximenes <92697766+pedroboseximenes@users.noreply.github.com>
2022-01-08 17:49:37 -0300

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,7 +1,8 @@
 from jogo import *
 
+
 def dificuldades(janela):
-    ##setando nome da janela, a imagem do fundo, botao easy,medio,hard, setando mouse e teclado:
+    """setando nome da janela, a imagem do fundo, botao easy,medio,hard, setando mouse e teclado:"""
 
     global level
     janela.set_title("Dificuldades!")
@@ -14,34 +15,33 @@ def dificuldades(janela):
 
     #################################
 
-    ##setando as posições dos botões de cada dificuldade!:
+    """setando as posições dos botões de cada dificuldade!:"""
 
-    botaoe.x = janela.width/5 - botaoe.width/2
-    botaoe.y = janela.height/2 - botaoe.height/2
-    botaom.x = janela.width/2 - botaom.width/2
-    botaom.y = janela.height/2 - botaom.height/2
-    botaoh.x = janela.width/1.25 - botaoh.width/2
-    botaoh.y = janela.height/2 - botaoh.height/2
+    botaoe.x = janela.width / 5 - botaoe.width / 2
+    botaoe.y = janela.height / 2 - botaoe.height / 2
+    botaom.x = janela.width / 2 - botaom.width / 2
+    botaom.y = janela.height / 2 - botaom.height / 2
+    botaoh.x = janela.width / 1.25 - botaoh.width / 2
+    botaoh.y = janela.height / 2 - botaoh.height / 2
 
     ##############################
 
-
     while True:
-        ##setanndo se clicar esc volta;
+        """setanndo se clicar esc volta;"""
         if (teclado.key_pressed("ESC")):
             break
-        ##setando se clicar no botão easy:
+        """setando se clicar no botão easy:"""
         if (mouse.is_button_pressed(1)) and (mouse.is_over_object(botaoe)):
             return 2
-        ##setando se clicar no botão medio:
+        """setando se clicar no botão medio:"""
         if (mouse.is_button_pressed(1)) and (mouse.is_over_object(botaom)):
             return 2
-        ##setando se clicar no botão hard:
+        """setando se clicar no botão hard:"""
         if (mouse.is_button_pressed(1)) and (mouse.is_over_object(botaoh)):
             return 3
         #################################
 
-        #desenhos do fundo e dos botões:
+        """desenhos do fundo e dos botões:"""
         fundo.draw()
         botaoe.draw()
         botaom.draw()
@@ -50,9 +50,9 @@ def dificuldades(janela):
 
 
 def menu(level):
-    ##setando janela, mouse, fundo, 4 botões (jogar,dificuldades,ranking,sair):
+    """setando janela, mouse, fundo, 4 botões (jogar,dificuldades,ranking,sair):"""
 
-    janela = Window(1024,768)
+    janela = Window(1024, 768)
     janela.set_title("Space-Invaders")
     mouse = Window.get_mouse()
 
@@ -64,10 +64,10 @@ def menu(level):
 
     #################################
 
-    ##setando as coords dos botões:
+    """setando as coords dos botões:"""
 
-    botao.x = janela.width/2 - botao.width /2
-    botao.y = janela.height/4 - botao.height/2
+    botao.x = janela.width / 2 - botao.width / 2
+    botao.y = janela.height / 4 - botao.height / 2
 
     botao2.x = janela.width / 2 - botao2.width / 2
     botao2.y = janela.height / 2.6 - botao2.height / 2
@@ -81,26 +81,26 @@ def menu(level):
     #################################
 
     while True:
-        ##verificar se mouse esta pressionado e sobre botao, logo vai p jogar:
+        """verificar se mouse esta pressionado e sobre botao, logo vai p jogar:"""
         if (mouse.is_button_pressed(1)):
             if (mouse.is_over_object(botao)):
-                jogar(janela,level)
-        ##verificar se mouse esta pressionado e sobre botao, logo vai p dificuldade:
+                jogar(janela, level)
+        """verificar se mouse esta pressionado e sobre botao, logo vai p dificuldade:"""
         if (mouse.is_over_object(botao2)):
             if (mouse.is_button_pressed(1)):
                 level = dificuldades(janela)
-        ##verificar se mouse esta pressionado e sobre botao, logo vai p ranking:
+        """verificar se mouse esta pressionado e sobre botao, logo vai p ranking:"""
         if (mouse.is_over_object(botao3)):
             if (mouse.is_button_pressed(1)):
                 None
-        ##verificar se mouse esta pressionado e sobre botao, logo vai p saida:
+        """verificar se mouse esta pressionado e sobre botao, logo vai p saida:"""
         if (mouse.is_over_object(botao4)):
             if (mouse.is_button_pressed(1)):
                 janela.close()
 
         #################################
 
-        ##desenhos:
+        """desenhos:"""
         fundo.draw()
         botao.draw()
         botao2.draw()
